9b3a3e1f7ab50e96d8327ef0723c96f204fc7f3d
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-03-11 11:26:58 -0400

    Time: 4 ms (12.97%) | Memory: 19 MB (47.78%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -17,8 +17,8 @@ class Solution:
 
             if node:
                 res = max(res, depth)
-                stack.append([node.left , depth+1])
-                stack.append([node.right, depth+1])
+                stack.append([node.right , depth+1])
+                stack.append([node.left, depth+1])
         return res
             
 
