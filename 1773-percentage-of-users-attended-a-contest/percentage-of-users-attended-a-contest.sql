197dee5e3b57feb0fc0de8d531a9e57f767bc0bd
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-10-19 18:06:31 -0400

    Time: 881 ms (77.96%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,25 +1,21 @@
--- # Write your MySQL query statement below
--- with userpercontest  as 
--- (
--- select contest_id , count(user_id) as usercnt
--- from register 
--- group by(contest_id)
+# Write your MySQL query statement below
+with userpercontest  as 
+(
+select contest_id , count(user_id) as usercnt
+from register 
+group by(contest_id)
 
--- ),
+),
 
--- countuser as (
--- select count(user_id) as total_user_sum
--- from users
--- )
+countuser as (
+select count(user_id) as total_user_sum
+from users
+)
 
--- select upc.contest_id as contest_id, Round( upc.usercnt*100/(select total_user_sum from countuser), 2) as percentage 
--- from  userpercontest upc
--- order by Round( upc.usercnt*100/(select total_user_sum from countuser), 2) desc ,contest_id  asc
-
-
+select upc.contest_id as contest_id, Round( upc.usercnt*100/(select total_user_sum from countuser), 2) as percentage 
+from  userpercontest upc
+order by Round( upc.usercnt*100/(select total_user_sum from countuser), 2) desc ,contest_id  asc
 
 
-SELECT b.contest_id, ROUND((count(distinct(b.user_id))*100/count(distinct(a.user_id))), 2) as percentage
-FROM Users as a, Register as b
-GROUP BY b.contest_id
-ORDER by percentage DESC, b.contest_id ASC
+
+
