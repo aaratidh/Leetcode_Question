b23b5e577d390d249de13636c0f4a3e37e771887
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-10-21 15:27:03 -0400

    Time: 557 ms (52.35%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+with cte2 as (
+select  player_id,min( event_date) as firstlog 
+from Activity 
+group by (player_id)
+
+
+)
+
+Select  Round(count(*)/(select count(distinct player_id) from Activity ),2) as  fraction  
+from activity a2
+join cte2 a1
+on a1.firstlog  + INTERVAL 1 DAY = a2.event_date  and a1.player_id  = a2.player_id 
