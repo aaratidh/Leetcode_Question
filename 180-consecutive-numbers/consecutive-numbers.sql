212bc38c56776ffd356a033786058e6fa92ecbdb
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-10-23 14:42:04 -0400

    Time: 557 ms (39.02%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
 with cte as (
 select  id  , num , 
-lead(num) over(order by id)  as led,
-lag(num)over (order by id ) as lg
-
+lead(num) over (order  by  id) as led,
+lag(num)over (order  by id ) as lg
 from  Logs  
 )
-
-
 
 select distinct num as ConsecutiveNums 
 from cte 
