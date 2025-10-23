d15e84c9d214d03080686a5c706c8dd6fa6c9e6e
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-10-23 17:15:14 -0400

    Time: 480 ms (50.75%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,19 +1,18 @@
-SELECT
-  p1.product_id,
-  COALESCE(
-    MAX(CASE
-          WHEN p1.change_date = (
-            SELECT MAX(p2.change_date)
-            FROM Products p2
-            WHERE p2.product_id = p1.product_id
-              AND p2.change_date <= '2019-08-16'
-          )
-          THEN p1.new_price
-        END),
-    10
-  ) AS price
-FROM Products p1
-GROUP BY p1.product_id;
+WITH t1 AS
+(SELECT DISTINCT product_id, MAX(change_date) over (PARTITION BY product_id) last_day
+FROM Products
+WHERE change_date<='2019-08-16' )
 
+SELECT t1.product_id, p.new_price price
+FROM t1
+JOIN Products p
+ON t1.product_id=p.product_id AND t1.last_day=p.change_date
 
+UNION
+
+SELECT product_id, 10 price
+FROM Products
+WHERE product_id NOT IN (SELECT product_id FROM t1)
+
+ 
 
