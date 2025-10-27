3c3488a71cfe68300edbd7d4dd1f5ab06a578c44
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-10-27 14:35:58 -0400

    Time: 781 ms (59.60%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -14,6 +14,8 @@ lead(Total_Weight) OVER (Order by Turn) As lead_weight
 from cte
 )
 
+
+
 select  Name  as person_name 
 from cte2 
 where (lead_weight> 1000 or lead_weight is NULL ) and  Total_Weight <=1000 
