f25adffc4d8dc42a235c4dd64e31602586663c47
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-11-10 19:56:50 -0500

    Time: 414 ms (89.75%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,18 +1,18 @@
-WITH t1 AS
-(SELECT DISTINCT product_id, MAX(change_date) over (PARTITION BY product_id) last_day
-FROM Products
-WHERE change_date<='2019-08-16' )
+with t1 as (
+select  distinct  product_id as product_id ,  max(change_date) as change_date
+from products  
+where  change_date <= '2019-08-16'
+group by product_id 
+)
 
-SELECT t1.product_id, p.new_price price
-FROM t1
-JOIN Products p
-ON t1.product_id=p.product_id AND t1.last_day=p.change_date
+select  p.product_id  as product_id , p.new_price  as price 
+from products  p 
+join t1 
+on t1.product_id = p.product_id and t1.change_date = p.change_date
 
-UNION
+union 
 
-SELECT product_id, 10 price
-FROM Products
-WHERE product_id NOT IN (SELECT product_id FROM t1)
-
- 
-
+select  product_id , 10 as price 
+from products 
+where  product_id not in (  select product_id from t1)
+order by product_id
