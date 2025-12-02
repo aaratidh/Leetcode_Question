48748d5d01b279d1fa625ec2b10688c053863b86
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-12-02 16:55:01 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (12.19%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,14 +1,8 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        bool_arry = [0] * len(candies)
         max_no_of_Candies = max(candies)
-        for i  in  range(0,len(candies)):
-            if candies[i] + extraCandies >=  max_no_of_Candies: 
-                bool_arry[i] = True
-            else:
-                bool_arry[i] = False 
-        return bool_arry
-
+        return [(curr + extraCandies >= max_no_of_Candies) for curr in candies]
+       
 
 
 
