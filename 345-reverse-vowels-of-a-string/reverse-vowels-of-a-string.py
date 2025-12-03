8334916b43b61ec893ef737cfa13f9e4390ae077
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-12-03 13:42:42 -0500

    Time: 19 ms (28.11%) | Memory: 18.7 MB (47.45%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -2,20 +2,41 @@ class Solution:
     def reverseVowels(self, s: str) -> str:
         list1 = list(s)
         alphabet = ['a','e','i','o','u' ,'A', 'E','I','O','U']
-        alph_in_list = []
-        #check for alphabet
+        stack = []
+        for letter in list1: 
+            if letter in alphabet:
+                stack.append(letter)
         for i, letter in enumerate(list1):
             if letter in alphabet:
-                alph_in_list.append(letter)
-        #reverse_alphabet_order
-        rev = alph_in_list[::-1]
-        #replace 
-        j = 0 
-        for i , letter in enumerate(list1):
-            if letter in alphabet:
-                list1[i] = rev[j]
-                j = j + 1
-        return("".join(list1))
+                list1[i]  = stack.pop()
+        return "".join(list1)
+
+
+
+        
+
+
+
+
+
+
+    #solution for O(n)
+        # list1 = list(s)
+        # alphabet = ['a','e','i','o','u' ,'A', 'E','I','O','U']
+        # alph_in_list = []
+        # #check for alphabet
+        # for i, letter in enumerate(list1):
+        #     if letter in alphabet:
+        #         alph_in_list.append(letter)
+        # #reverse_alphabet_order
+        # rev = alph_in_list[::-1]
+        # #replace 
+        # j = 0 
+        # for i , letter in enumerate(list1):
+        #     if letter in alphabet:
+        #         list1[i] = rev[j]
+        #         j = j + 1
+        # return("".join(list1))
 
             
 
