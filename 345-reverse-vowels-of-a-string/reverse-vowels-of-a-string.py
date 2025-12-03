9cc90b1e79b90c89f553628556efe468622a188d
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-12-03 13:46:42 -0500

    Time: 8 ms (72.16%) | Memory: 18.7 MB (28.53%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -1,23 +1,47 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        list1 = list(s)
-        alphabet = ['a','e','i','o','u' ,'A', 'E','I','O','U']
-        stack = []
-        for letter in list1: 
-            if letter in alphabet:
-                stack.append(letter)
-        for i, letter in enumerate(list1):
-            if letter in alphabet:
-                list1[i]  = stack.pop()
-        return "".join(list1)
+        vowels = set("aeiouAEIOU")
+        s_list = list(s)
 
+        left, right = 0, len(s_list) - 1
 
-
+        while left < right:
+            # move left pointer until vowel
+            while left < right and s_list[left] not in vowels:
+                left += 1
+            
+            # move right pointer until vowel
+            while left < right and s_list[right] not in vowels:
+                right -= 1
+            
+            # swap vowels
+            s_list[left], s_list[right] = s_list[right], s_list[left]
+            
+            left += 1
+            right -= 1
         
+        return "".join(s_list)
+            
 
-
-
-
+               
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    #Solution using Stack    
+        # for letter in list1: 
+        #     if letter in alphabet:
+        #         stack.append(letter)
+        # for i, letter in enumerate(list1):
+        #     if letter in alphabet:
+        #         list1[i]  = stack.pop()
+        # return "".join(list1)
 
 
     #solution for O(n)
