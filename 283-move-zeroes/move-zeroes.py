a91cc63a74917c62cf580a602cd8e89760b53e04
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-12-08 18:31:20 -0500

    Time: 0 ms (100.00%) | Memory: 18.8 MB (49.43%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -9,23 +9,6 @@ class Solution:
             nums[i] = 0 
 
 
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # class Solution:
@@ -33,12 +16,9 @@ class Solution:
 #         l= 0 
 #         r = len(nums) - 1
 #         while l < r:
-#             if nums[l] == 0 and nums[r] == 0:
+#             if nums[r] == 0:
 #                 r = r-1
-#             elif nums[l] != 0 and nums[r] == 0:
-#                 r = r-1  
-#                 l = l-1
-#             elif nums[l] != 0 and nums[r] != 0: 
+#             elif nums[l] != 0: 
 #                 l = l+1
 #             else:
 #                 nums[l], nums[r] = nums[r] , nums[l]
@@ -46,33 +26,34 @@ class Solution:
 #                 r = r - 1
 
 #         def mergesort(arry):
-#             if len(arry)> 1:
-#                 left_arry =  arry[:len(arry)//2]
-#                 right_arry = arry[len(arry)//2:]
-#                 mergesort(left_arry)
-#                 mergesort(right_arry)
+#             if len(arry)<=1:
+#                 return arry
+
+#             mid = len(arry)//2 
+#             left_arry = mergesort( arry[:mid])
+#             right_arry = mergesort( arry[mid:])
 #             #merge 
-#                 i = 0 
-#                 j = 0 
-#                 k = 0 
-#                 while  i < len(left_arry) and j < len(right_arry): 
-#                     if left_arry[i] > right_arry[j]: 
-#                         arry[k] = right_arry[j]
-#                         j = j + 1
-#                     else: 
-#                         arry[k] = left_arry[i]
-#                         i = i+ 1 
-#                     k = k + 1 
-#                 while i < len(left_arry): 
+#             i = 0 
+#             j = 0 
+#             k = 0 
+#             while  i < len(left_arry) and j < len(right_arry): 
+#                 if left_arry[i] <= right_arry[j]: 
 #                     arry[k] = left_arry[i]
 #                     i = i + 1
-#                     k = k+ 1
-#                 while j < len(right_arry): 
+#                 else: 
 #                     arry[k] = right_arry[j]
-#                     k = k + 1
-#             print(arry)
+#                     j = j+ 1 
+#                 k = k + 1 
+#             while i < len(left_arry): 
+#                 arry[k] = left_arry[i]
+#                 i = i + 1
+#                 k = k+ 1
+#             while j < len(right_arry): 
+#                 arry[k] = right_arry[j]
+#                 k = k + 1
+#                 j = j + 1   
 #             return arry
-#         nums= mergesort(nums[0:r+1]) + nums[r+1:]
+#         nums[:] = mergesort(nums[:r+1]) + nums[r+1:]
 #         return nums
 
 
