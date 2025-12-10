38a4da619c8a504c317d4dcc7f0cfa2c3fc74c53
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-12-10 18:13:01 -0500

    Time: 115 ms (16.24%) | Memory: 27.2 MB (83.77%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -4,18 +4,13 @@ class Solution:
         j = k
         maxsum = float('-inf')
         sum1 = sum(nums[i:j])
-        maxsum = maxsum = max(sum1 , maxsum)
+        maxsum = max(sum1 , maxsum)
         while j <= len(nums)-1:
             sum1 = sum1 + nums[j] - nums[i]
             maxsum = max(sum1 , maxsum)
             i = i + 1 
             j = j + 1
         return maxsum/k
-
-
-
-
-
         
             
 
