1bce5cb60255904952ecfb424c43722b0dd4e3e6
Aarati Dhungel <35992124+aaratidh@users.noreply.github.com>
2025-12-11 17:05:18 -0500

    Time: 503 ms (62.62%) | Memory: 29.8 MB (79.45%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,20 +1,59 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        total = 0                          # how many pairs we found
-        counter = collections.Counter()    # numbers we have waiting for a match
+        nums.sort()
+        i, j = 0, len(nums) - 1
+        ops = 0
+    
+        while i < j:
+            s = nums[i] + nums[j]
+            if s == k:
+                ops += 1
+                i += 1
+                j -= 1
+            elif s < k:
+                i += 1
+            else:  # s > k
+                j -= 1
+    
+        return ops
 
-        for x in nums:
-            # complement we need to form k
-            if k - x in counter:           # have we already seen a number = k - x?
-                total += 1                 # yes → we can form a pair
-                counter[k - x] -= 1        # use one copy of that complement
-                if counter[k - x] == 0:    # if no more copies left
-                    del counter[k - x]     # remove it from the counter
-                continue                   # go to next number
 
-            # otherwise, save this number to maybe pair later
-            counter[x] += 1
+# [3,1,3,4,3]
+#  i i    j-1 j
 
-        return total
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def maxOperations(self, nums: List[int], k: int) -> int:
+    #     total = 0                          # how many pairs we found
+    #     counter = collections.Counter()    # numbers we have waiting for a match
+
+    #     for x in nums:
+    #         # complement we need to form k
+    #         if k - x in counter:           # have we already seen a number = k - x?
+    #             total += 1                 # yes → we can form a pair
+    #             counter[k - x] -= 1        # use one copy of that complement
+    #             if counter[k - x] == 0:    # if no more copies left
+    #                 del counter[k - x]     # remove it from the counter
+    #             continue                   # go to next number
+    #         # otherwise, save this number to maybe pair later
+    #         counter[x] += 1
+
+    #     return total
 
   
